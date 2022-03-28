826e69fec2e39fbdb58c1616186a881f3ec30f3d
Raymond Doetjes <rdoetjes@phonax.com>
2022-03-28 12:59:46 +0200

    refactored

--- knightrider.c
+++ knightrider.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+
+#define DELAY_US 50000
+
 void openDevice(int ipid){
     if ( OpenDevice(ipid)<0 ) {
 		printf("Could not open the k8055 (port:%d)\nPlease ensure that the device is correctly connected.\n",ipid);
@@ -11,35 +14,28 @@ void openDevice(int ipid){
             
 }
 
-#define DELAY_US 50000
+void knightRiderScan(int up){
+  static int dout = 1;
 
+  for(int i=0; i<7; ++i){
+    WriteAllDigital((long)dout);
+    OutputAnalogChannel(1, (long)dout);
+    OutputAnalogChannel(2, (long)dout);
+    if ( ReadAllDigital() >0){
+      CloseDevice();
+      exit(0);
+    }
+    
+    dout = up ? dout << 1 : dout >> 1;
+    usleep(DELAY_US);
+  }
+}
 int main(){
-    static int dout = 1;
+ 
     openDevice(0);
 
     while(1){
-        for(int i=0; i<7; ++i){
-          WriteAllDigital((long)dout);
-          OutputAnalogChannel(1, (long)dout);
-          OutputAnalogChannel(2, (long)dout);
-          if ( ReadAllDigital() >0){
-            CloseDevice();
-            exit(0);
-          }
-          dout = dout << 1;
-          usleep(DELAY_US);
-        }
-
-        for(int i=0; i<7; i++){
-          WriteAllDigital((long)dout);
-          OutputAnalogChannel(1, (long)dout);
-          OutputAnalogChannel(2, (long)dout);
-          if ( ReadAllDigital() >0){
-            CloseDevice();
-            exit(0);
-          }
-          dout = dout >> 1;
-          usleep(DELAY_US);
-        }
+      knightRiderScan(1);
+      knightRiderScan(0);
     }
 }
